2742ae0a447fff859a617c9fad24db7943e5a30f
firman18520017 <firman_18520017@stimata.ac.id>
2021-12-10 20:15:53 +0700

    Assigment-3

---
+++ Tugas1.js
@@ -0,0 +1,14 @@
+let recipe = ["Didihkan air dlm panci,beri sedikit garam dan 1 sdm minyak goreng,rebus spaghetti hingga empuk,krg lebih 15 menit", "Panaskan butter,tumis bawang putih dan bawang bombay hingga harum,masukan smoked beef aduk rata,tuang susu cair dan keju aduk rata,lalu beri garam,lada,peterseli,oregano aduk rata test rasanya yah jika sdh pas masukan spaghetti aduk rata lalu tuang larutan maizena aduk rata,sy ga pake peterseli krn ga punya", "Terakhir kocok telur dan campur dgn sedikit susu cair ambil dr bahan pisahin sedikit untuk kocokan telur,sedikit aja yah...masukan kocokan telur aduk2 lalu angkat dan sajikan 😊 telur jgn terlalu matang yah nanti krg creamy"];
+
+console.log(recipe[0]); // Akses step pertama.
+
+var last_step = recipe[2];
+console.log(last_step); // Step terakhir dari resep
+
+
+let recipe=["Pertama panas kanair","Selanjutnya, sambil menunggu mendidih campurkan 1 bungkus agar-agar dan gula pasir kedalam air yang dipanaskan","Setelah itu, potong buah-buahan menjadi kecil (sesuai selera)","lalu, letakkan buah kedalam loyang/ cetakan (sesuai selera)","Kemudian, jika air yang dipanaskan sudah mendidih, segera tuangkan ke dalam cetakan/ loyang yang sudah diberi kombinasi buah.","Berikutnya, diamkan hingga dingin.","Langkah selanjutnya, simpan di lemari es agar rasa lebih lezat.","Terakhir, ambil agar-agar dari lemari pendingin dan santap."];
+
+let last_step=recipe[7];
+console.log("Langkah-langkah dalam membuat agar-agar campur buah");
+console.log("Langkah awal :" +recipe[0]);
+console.log("Langkah akhir :" +last_step);
---
+++ Tugas2.js
@@ -0,0 +1,23 @@
+let my_bucket_list =["Travel Toba lake","Run a Marathon","Designing website"];
+my_bucket_list.push("watching movies");
+my_bucket_list.unshift("Go camping");
+my_bucket_list.pop();
+my_bucket_list.shift();
+
+
+
+console.log("My Bucket list :" +"",my_bucket_list);
+
+
+let my_bucket_list = ["Pergi ke Bali", "Pergi ke Singapore"];
+
+//push
+my_bucket_list.push("Masak Sushi");
+//unshift
+my_bucket_list.unshift("Pulang Kampung ke Medan");
+//pop
+my_bucket_list.pop();
+//shift
+my_bucket_list.shift();
+
+console.log(my_bucket_list);
